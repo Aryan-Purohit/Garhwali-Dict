dbc1efb1814d38024941ed8d397b042d8f34062c
Aryan-Purohit <aryanpurohit007@outlook.com>
2024-08-04 16:37:26 -0700

    Added functions

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import mysql
 
 def main():
     number = int(input("Enter word"))
+
+def addWord():
+    num = 0
     
+def findEntry():
+    n = 0
+
+def removeEntry():
+    nu = 0
+
 if __name__ == "__main__":
     main()
 
