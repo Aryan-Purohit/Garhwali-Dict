c47c2951c2c9775afbf3a0fed1830f3b8ca9ea25
Aryan-Purohit <aryanpurohit007@outlook.com>
2024-08-05 22:12:21 -0700

    Updated Main.py
    Added find word capability(for english only)

--- main.py
+++ main.py
@@ -4,52 +4,53 @@ from mysql.connector import Error
 
 def main():
     
+    connection = create_connection()
     choice = int(input("Enter your input.\n1. Find\n2. Add word\n3. Remove\n"))
     
     if choice == 1:
-        findEntry()
-    elif choice == 2:
-        addWord()
+        word = input("Enter English word ")
+        findEntry(connection, word)
+    elif choice == 2:   
+        addWord(connection, word)
     elif choice == 3:
-        removeEntry()
+        removeEntry(connection)
     else:
-        print("Error. You entered a wrong choice")       
-
-def addWord():
-    num = 0
+        print("Error. You entered a wrong choice")    
     
-def findEntry():
+
+def addWord(connection):
+    cursor = connection.cursor()
+    
+def findEntry(connection, word):
+    cursor = connection.cursor()
+    
+    
+    query = "SELECT * FROM dictionary WHERE English_word = %s"
+    cursor.execute(query, (word,))
+    
+    output = cursor.fetchall()
+    
+    for row in output:
+        print(f"Hindi_word: {row[2]}, Garhwali_word: {row[1]}")
+
+def removeEntry(connection):
+    cursor = connection.cursor()
+ 
+#Establishes the connection to SQL database    
+def create_connection():
+    connection = None
     try:
-        conn = mysql.connector.connect(
+        connection = mysql.connector.connect(
             host = 'localhost',
             user = 'root',
             password = 'Aryu@118',
             database = 'Garhwali_Dict'
         )
-
-        if conn.is_connected():
-            print("Connected to mySQL database")
-        
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM dictionary")
-        rows = cursor.fetchall()
-    
-        for r in rows:
-            print(f"Garhwali: {r[1]}, Hindi: {r[2]}, English: {r[3]}")
-    
-
-    except Error as e :
-        print(f"Error: {e}")   
-        
-    finally:
-        if cursor:
-            cursor.close()
-        
-        if conn and conn.is_connected():
-            conn.close()
-
-def removeEntry():
-    nu = 0
+        print("Connection to database successful")
+    except Error as e:
+        print(f"The Error '{e}' occurred")
+     
+    return connection
 
 if __name__ == "__main__":
     main()
